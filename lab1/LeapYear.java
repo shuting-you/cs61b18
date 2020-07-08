5fe170eb93fe3b8d6f7d5dcf663a0bac7b0984a8
Shuting You <shutingy@umich.edu>
2020-07-08 14:39:29 -0400

    finished LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,14 @@ public class LeapYear {
         }
     }
 
+    private static boolean isLeapYear(int y){
+	if(y % 400 == 0){
+		return true;
+	}else if((y % 100 != 0) && (y % 4 == 0)){
+		return true;
+	}else return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
