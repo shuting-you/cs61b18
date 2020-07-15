b18c6735966ddce4c3fdd8bf185d926eab6e62a5
Shuting You <shutingy@umich.edu>
2020-07-14 20:33:42 -0400

    check the update() method

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,6 +6,7 @@ public class Planet {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+	private static final double G = 6.67e-11;
 
 	public Planet(double xP, double yP, double xV, 
 		double yV, double m, String img) {
@@ -27,9 +28,55 @@ public class Planet {
 	}
 
 	public double calcDistance(Planet p) {
-		double dx = Math.abs(this.xxPos - p.xxPos);
-		double dy = Math.abs(this.yyPos - p.yyPos);
+		double dx = this.xxPos - p.xxPos;
+		double dy = this.yyPos - p.yyPos;
 		return Math.sqrt(dx * dx + dy * dy); 
+	}
+
+	public double calcForceExertedBy(Planet p) {
+		if (calcDistance(p) == 0) 
+			return 0;
+		return G * this.mass * p.mass / (calcDistance(p) * calcDistance(p));
+
+	}
+
+	public double calcForceExertedByX(Planet p) {
+		double dx = p.xxPos - xxPos;
+		return calcForceExertedBy(p) * dx / calcDistance(p);
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		double dy = p.yyPos - yyPos;
+		return calcForceExertedBy(p) * dy /calcDistance(p);
+	}
+
+	public double calcNetForceExertedByX(Planet[] arr) {
+		double netForce = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (!this.equals(arr[i]))
+			netForce += calcForceExertedByX(arr[i]);
+		}
+		return netForce;
+	}
+
+	public double calcNetForceExertedByY(Planet[] arr) {
+		double netForce = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (!this.equals(arr[i]))
+			netForce += calcForceExertedByY(arr[i]);
+		}
+		return netForce;
+	}
+
+	public void update(double dt, double fX, double fY) {
+		double aX = fX / mass;
+		double aY = fY / mass;
+
+		xxVel += aX * dt;
+		yyVel += aY * dt;
+
+		xxPos += xxVel * dt;
+		yyPos += yyVel * dt;
 	}
 
 
