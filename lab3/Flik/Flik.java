024ec1fe57e4c31844cebd7c23a35dc9a2de0288
Shuting You <shutingy@umich.edu>
2020-08-08 15:05:53 -0400

    test passed

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
         return a == b;
+    }
+
+    @Test
+    public void testFlik() {
+        boolean A = isSameNumber(2, 2);
+        assertTrue(A);
     }
 }
