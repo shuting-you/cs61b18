87ad28bbc0bc47f848689e5a146a92384c2ec6f1
Shuting You <shutingy@umich.edu>
2020-08-08 15:13:00 -0400

    find the error

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
+        // Integer from 128 starts to store in different places
         return a == b;
     }
 
